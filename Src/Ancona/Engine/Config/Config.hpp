780c18f633bf07e0d89ba52c987c83f7cd1a2676
Jeff Swenson <jeffpswenson@gmail.com>
2014-09-08 20:25:27 -0500

    Add comments to Config.hpp

--- Src/Ancona/Engine/Config/Config.hpp
+++ Src/Ancona/Engine/Config/Config.hpp
@@ -6,12 +6,29 @@
 namespace ild
 {
 
+/**
+ * @brief Used to load and store configuration options.  Load should be the first method called in
+ * the ancona engine.
+ * @owner Jeff Swenson
+ */
 class Config 
 {
     public:
 
+        /**
+         * @brief Load the setting file and store the options.
+         *
+         * @param configFile A string containing the configuration file name.
+         */
         static void Load(const std::string & configFile);
 
+        /**
+         * @brief Get option from the configuration settings.
+         *
+         * @param optionName Name of the option.
+         *
+         * @return Value of the option.  If the value was not set the "" is returned.
+         */
         static const std::string & GetOption(const std::string & optionName);
 
     private:
