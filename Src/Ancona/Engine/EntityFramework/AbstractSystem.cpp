0061248a4e8d3eded09454317fe366ceae5c72c9
Jeff Swenson <jeffpswenson@gmail.com>
2014-09-11 20:44:16 -0500

    Finish Entity Component System

---
+++ Src/Ancona/Engine/EntityFramework/AbstractSystem.cpp
@@ -0,0 +1,11 @@
+#include <Ancona/Engine/EntityFramework/AbstractSystem.hpp>
+#include <Ancona/Engine/EntityFramework/SystemManager.hpp>
+#include <Ancona/Engine/EntityFramework/UpdateStep.hpp>
+
+using namespace ild;
+
+AbstractSystem::AbstractSystem(SystemManager & systemManager, UpdateStepEnum updateStep) : 
+    _systemManager(systemManager)
+{
+    systemManager.RegisterSystem(this, updateStep);
+}
---
+++ Src/Ancona/Engine/EntityFramework/UpdateStep.hpp
@@ -0,0 +1,34 @@
+#ifndef Ancona_Engine_EntityFramework_UpdateStep_H_
+#define Ancona_Engine_EntityFramework_UpdateStep_H_
+
+namespace ild
+{
+
+namespace UpdateStep
+{
+    //The namespace is used to prevent the enum values from leaking into
+    //the ild namespace.  In order to use a enum value you type
+    //UpdateStep::Update or UpdateStep::Draw.  Without the namespace you 
+    //could have typed UpdateStep
+    
+    
+    /**
+     * @brief Defines when a system should be updated
+     * @owner Jeff Swenson
+     */
+    enum UpdateStep
+    {
+        Update,
+        Draw
+    };
+}
+
+/**
+ * @brief Defines when a system should be updated.
+ * @owner Jeff Swenson
+ */
+typedef UpdateStep::UpdateStep UpdateStepEnum;
+
+}
+
+#endif
