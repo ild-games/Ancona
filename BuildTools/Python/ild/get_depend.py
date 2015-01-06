079e9a8d860e7f3e3020e8d4fe3103eee8ecb9bc
Jeff Swenson <jeffpswenson@gmail.com>
2015-01-05 19:10:44 -0600

    Make progress on the build system for android

---
+++ BuildTools/Python/ild/get_depend.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+##
+# @brief Script used to download and build project dependencies
+# @author Jeff Swenson
+#
+# usage: python3 get_depend.py [cmake_dir] [platform]
+
+import os,sys
+
+#Add ancona python import to the search path
+#python_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+#sys.path.append(python_dir)
+
+from ild.building import *
+
+
+#Repo [url, tag]
+SFML_REPO = ["SFML", "http://github.com/LaurentGomila/SFML", "2.2"]
+
+
+def main(cmake_dir, platform):
+    toolchain = get_toolchain(cmake_dir, platform)
+
+    #Clone the SFML Repo
+    if not SFML_installed(get_lib_dir(cmake_dir,"SFML"),platform):
+        sfml_repo = get_git_repo(cmake_dir, *SFML_REPO)
+        build_SFML(sfml_repo,platform,toolchain)
+    else:
+        print("SFML is already installed for this platform")
+
+def get_build_dir(lib_src, platform):
+    return os.path.join(lib_src,"build",platform)
+
+def build_SFML(lib_src,platform,toolchain):
+    build_dir = get_build_dir(lib_src,platform)
+    build_cmake_project(lib_src,build_dir,toolchain,install=True)
+
+def SFML_installed(lib_src,platform):
+    return os.path.isdir(get_build_dir(lib_src,platform))
+        
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: {} [cmake_src_dir] [platform]".format(sys.argv[0]))
+        sys.exit(1)
+    main(os.path.abspath(sys.argv[1]),sys.argv[2])
--- README.md
+++ README.md
@@ -13,6 +13,19 @@ To get started on a Posix OS:
 	5. Run the command "cmake .."
 	6. Run the command "make"
 
+To build on Android:
+
+    1. Download the android NDK
+    2. Add ndk/tools to your PATH environment variable
+    3. Set the NDK and ANDROID_NDK environment variables to be the path to the android NDK
+    4. Clone SFML from github
+    5. Set the ANDROID_CMAKE_TOOLCHAIN environment variable to the toolchain file located in SFML/cmake/toolchains
+    6. Open a terminal and navigate to the SFML/tools/android environment
+    7. Run the make_all.sh script
+    8. Open a terminal in the ancona root
+    9. Run the ancona script in the root with "Android" as the argument
+    10. Change into the "build" directory and run make
+
 The binaries will be in the Build/bin folder.
 
 Coming Soon:
