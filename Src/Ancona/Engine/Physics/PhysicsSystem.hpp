62591d5d6b5b35717ad179180930fb433a01effa
Jeff Swenson <jeffpswenson@gmail.com>
2015-01-24 00:21:54 -0600

    Start work on the physics system

---
+++ Src/Ancona/Engine/Physics/PhysicsSystem.hpp
@@ -0,0 +1,40 @@
+#ifndef Ancona_Engine_Physics_PhysicsSystem_hpp
+#define Ancona_Engine_Physics_PhysicsSystem_hpp
+
+namespace ild
+{
+
+class PhysicsComponent
+{
+    public:
+        //SetVelocity()
+        //SetPosition()
+        //SetAcceleration()
+        //
+        //Acceleration()
+        //Position()
+        //Velocity()
+        //
+        //Properties
+        //  Priority()
+        //  Value()
+        //  Duration()
+        //
+        //Output
+        //    GetDirection
+        //    GetVelocity
+        //    GetPosition
+        //    GetAcceleration
+    private:
+};
+
+class PhysicsSystem
+{
+    public:
+    private:
+};
+
+}
+
+
+#endif
