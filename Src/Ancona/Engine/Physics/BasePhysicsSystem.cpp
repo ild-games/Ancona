3b152707334101ab35fa42fed3208592f4fb189d
Jeff Swenson <jeffpswenson@gmail.com>
2015-02-08 13:28:12 -0600

    Move physics system into Core/Systems

--- Src/Ancona/Engine/Physics/BasePhysicsSystem.cpp
+++
@@ -1,9 +0,0 @@
-#include <Ancona/Engine/Physics/BasePhysicsSystem.hpp>
-
-using namespace ild;
-
-BasePhysicsSystem::BasePhysicsSystem(SystemManager & manager)
-    : UnorderedSystem<BasePhysicsComponent>(manager, UpdateStep::Update)
-{
-
-}
