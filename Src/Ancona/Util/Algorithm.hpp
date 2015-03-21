06918132e5a9a41f38973b341ad15fc05f94dd6c
Jeff Swenson <jeffpswenson@gmail.com>
2015-03-21 18:13:48 -0500

    Finish container wrappers for read only functions.

---
+++ Src/Ancona/Util/Algorithm.hpp
@@ -0,0 +1,10 @@
+#ifndef Ancona_Util_Algorithm_H_
+#define Ancona_Util_Algorithm_H_
+
+#include <algorithm>
+#include <iterator>
+#include <util>
+
+#include <Ancona/Util/Algorithm/ContainerWrappers.hpp>
+
+#endif
