6298c3122bc3e4ba0e3491def0a6144bbca7e384
Jeff Swenson <jeffpswenson@gmail.com>
2015-04-17 23:45:04 -0500

    Modify unit tests to match the library reordering.

--- Test/Unit/AnconaTest/Util2/Collision/VectorMathTest.cpp
+++ Test/Unit/AnconaTest/Util2/Collision/VectorMathTest.cpp
@@ -1,8 +1,8 @@
-#include <gtest/gtest.h>
-
 #include <cmath>
 
-#include <Ancona/Util/VectorMath.hpp>
+#include <gtest/gtest.h>
+
+#include <Ancona/Util2D/VectorMath.hpp>
 #include <Ancona/Engine/Core/Systems/Physics/Position.hpp>
 
 
