b0126912cf8832dfbcc94d1c9ee2aa2c55688f18
Tucker Lein <tlein@radford.edu>
2015-05-22 22:09:21 -0500

    remove android build code

--- BuildTools/Python/ild/get_depend.py
+++ BuildTools/Python/ild/get_depend.py
@@ -17,34 +17,26 @@ from ild.building import *
 
 #Repo [name, url, tag (optional)]
 SFML_REPO = ["SFML", "git@bitbucket.org:JeffSwenson/sfml.git", "2.2"]
-JSONCPP_REPO = ["jsoncpp", "https://github.com/open-source-parsers/jsoncpp.git"]
 
 
 def main(cmake_dir, platform):
     toolchain = get_toolchain(cmake_dir, platform)
 
     #Clone the SFML Repo
-    if not dependency_installed(get_lib_dir(cmake_dir,"SFML"),platform):
+    if not SFML_installed(get_lib_dir(cmake_dir,"SFML"),platform):
         sfml_repo = get_git_repo(cmake_dir, *SFML_REPO)
-        build_dependency(sfml_repo,platform,toolchain)
+        build_SFML(sfml_repo,platform,toolchain)
     else:
         print("SFML is already installed for this platform")
-
-    #Clone the jsoncpp repo
-    if not dependency_installed(get_lib_dir(cmake_dir, "jsoncpp"), platform):
-        jsoncpp_repo = get_git_repo(cmake_dir, *JSONCPP_REPO)
-        build_dependency(jsoncpp_repo,platform,toolchain)
-    else:
-        print("jsoncpp is already installed for this platform")
 
 def get_build_dir(lib_src, platform):
     return os.path.join(lib_src,"build",platform)
 
-def build_dependency(lib_src,platform,toolchain):
+def build_SFML(lib_src,platform,toolchain):
     build_dir = get_build_dir(lib_src,platform)
     build_cmake_project(lib_src,build_dir,toolchain,install=True)
 
-def dependency_installed(lib_src,platform):
+def SFML_installed(lib_src,platform):
     return os.path.isdir(get_build_dir(lib_src,platform))
         
 
