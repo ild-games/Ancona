2a3b4ad8f418808166e65c03cb85dcb1c706eeaf
Tucker Lein <tuckerjlein@gmail.com>
2015-06-26 22:28:55 -0500

    fix pull request issues

--- BuildTools/Python/ild/duckling/template.py
+++ BuildTools/Python/ild/duckling/template.py
@@ -6,9 +6,12 @@ TEMPLATE_GIT_REPO = 'git@bitbucket.org:ilikeducks/ancona-template-game.git'
 
 def start_template(game_name, game_abbr):
     game_name = ''.join(game_name.split(' '))
-    prep_work()
-    templatize_project(game_name, game_abbr)
-    tear_down(game_name) 
+    try:
+        prep_work()
+        templatize_project(game_name, game_abbr)
+        create_prototype_folder(game_name)
+    finally:
+        clean_up(game_name) 
 
 def prep_work():
     if os.path.exists('__applied__'):
@@ -43,25 +46,20 @@ def apply_template_to_file(file, game_name, game_abbr):
     with open(file, "wt") as out_file:
         out_file.write(result)
 
-def append_prototype_to_cmake(folderName):
-    line_to_add = 'add_subdirectory(' + folderName + ')'
-    cmake_file = 'Test/Prototype/CMakeLists.txt'
-    if line_to_add not in open(cmake_file).read():
-        with open(cmake_file, 'a') as file:
-            file.write('\n' + line_to_add)
-
-def tear_down(game_name):
-    append_prototype_to_cmake(game_name)
+def create_prototype_folder(game_name):
     project_path = 'Test/Prototype/' + game_name + '/'
 
     keep_going = True
-    if os.path.exists(os.getcwd() + '/' + project_path):
+    if os.path.exists(os.path.join(os.getcwd(), project_path)):
         keep_going = sscript.input_yn('This prototype already exists, do you want to delete it and continue?')
         if keep_going:
             shutil.rmtree(project_path)
     if keep_going:
         shutil.copytree('__applied__', 'Test/Prototype/' + game_name)
 
-    shutil.rmtree('__template__')
-    shutil.rmtree('__applied__')
+def clean_up(game_name):
+    if os.path.exists('__template__'):
+        shutil.rmtree('__template__')
+    if os.path.exists('__applied__'):
+        shutil.rmtree('__applied__')
 
