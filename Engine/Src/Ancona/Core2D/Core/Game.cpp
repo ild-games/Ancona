cb2aac272779aeab1125d97e923c2a3389183983
Tucker <tlein@radford.edu>
2016-08-30 09:25:36 -0500

    add mouse input handler

--- Engine/Src/Ancona/Core2D/Core/Game.cpp
+++ Engine/Src/Ancona/Core2D/Core/Game.cpp
@@ -1,10 +1,11 @@
 #include <Ancona/Core2D/Core/Game.hpp>
 #include <Ancona/Framework/Screens/ScreenManager.hpp>
 #include <Ancona/Core2D/InputDevices/Keyboard.hpp>
+#include <Ancona/Core2D/InputDevices/Mouse.hpp>
 
 using namespace ild;
 
-Game::Game(int windowWidth, int windowHeight, const std::string & title) 
+Game::Game(int windowWidth, int windowHeight, const std::string & title)
     : _window(sf::VideoMode(windowWidth, windowHeight), title)
 {
     _screenManager = new ScreenManager(_window);
@@ -18,30 +19,43 @@ void Game::Run()
     while(_window.isOpen() && !_screenManager->Empty())
     {
         sf::Event event;
-        Keyboard::_ClearKeys(); 
+        Keyboard::_ClearKeys();
+        Mouse::_ClearButtons();
         while(_window.pollEvent(event))
         {
-
-            if(event.type == sf::Event::Closed)
-            {
-                _window.close();
-            }
-            if(event.type == sf::Event::KeyPressed)
-            {
-                Keyboard::_AddKeyPress(event.key.code); 
-            }
-            if(event.type == sf::Event::KeyReleased)
-            {
-                Keyboard::_AddKeyRelease(event.key.code);
-            }
+            ProcessWindowEvent(event);
         }
 
         sf::Time elapsed = clock.restart();
         float delta = elapsed.asSeconds();
         _screenManager->Update(delta);
-            
+
         _window.clear(sf::Color::Black);
         _screenManager->Draw(delta);
         _window.display();
+    }
+}
+
+void Game::ProcessWindowEvent(sf::Event event)
+{
+    if(event.type == sf::Event::Closed)
+    {
+        _window.close();
+    }
+    if(event.type == sf::Event::KeyPressed)
+    {
+        Keyboard::_AddKeyPress(event.key.code);
+    }
+    if(event.type == sf::Event::KeyReleased)
+    {
+        Keyboard::_AddKeyRelease(event.key.code);
+    }
+    if (event.type == sf::Event::MouseButtonPressed)
+    {
+        Mouse::_AddButtonPress(event.mouseButton.button);
+    }
+    if (event.type == sf::Event::MouseButtonReleased)
+    {
+        Mouse::_AddButtonRelease(event.mouseButton.button);
     }
 }
