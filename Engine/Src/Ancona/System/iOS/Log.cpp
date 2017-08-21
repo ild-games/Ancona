11555ef3153be49baac172cd5362ccb22795bf7f
tlein <tlein@radford.edu>
2017-08-20 22:55:29 -0500

    get Ancona building for iOS

---
+++ Engine/Src/Ancona/System/iOS/Log.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <Ancona/System/Log.hpp>
+
+using namespace ild;
+
+void LogControls::_log(const std::string & msg)
+{
+    std::cout << "[Ancona] " << msg << std::endl;
+}
