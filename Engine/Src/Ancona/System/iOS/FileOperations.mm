13be6f532b89000f0bf9d2a971726d22ca73535b
tlein <tlein@radford.edu>
2017-10-22 18:16:47 -0500

    add iOS file operations necessary for saving

--- Engine/Src/Ancona/System/iOS/FileOperations.mm
+++ Engine/Src/Ancona/System/iOS/FileOperations.mm
@@ -1,6 +1,7 @@
 #include <fstream>
 
 #include <Ancona/System/FileOperations.hpp>
+#include <Ancona/System/iOS/iOSFileOperations.hpp>
 #include <Ancona/System/Log.hpp>
 
 #import <Foundation/Foundation.h>
@@ -9,14 +10,22 @@ using namespace ild;
 
 std::unique_ptr<std::istream> FileOperations::GetInputFileStream(const std::string & desiredFile)
 {
-    auto fullFile = FileOperations::ResourceRoot() + desiredFile;
-    ILD_Log(fullFile);
-    return std::unique_ptr<std::istream>(new std::ifstream(fullFile, std::ifstream::binary));
+    if (iOSFileOperations::IsFileInDocumentsFolder(desiredFile))
+    {
+        auto fileInDocuments = iOSFileOperations::DocumentsRoot() + desiredFile;
+        ILD_Log("In documents: " + fileInDocuments);
+        return std::unique_ptr<std::istream>(new std::ifstream(fileInDocuments, std::ifstream::binary));
+    }
+
+    auto fileInAppBundle = FileOperations::ResourceRoot() + desiredFile;
+    ILD_Log("In app bundle: " + fileInAppBundle);
+    return std::unique_ptr<std::istream>(new std::ifstream(fileInAppBundle, std::ifstream::binary));
 }
 
 std::unique_ptr<std::ofstream> FileOperations::GetOutputFileStream(const std::string & desiredFile)
 {
-    auto fullFile = FileOperations::ResourceRoot() + desiredFile;
+    auto fullFile = iOSFileOperations::DocumentsRoot() + desiredFile;
+    ILD_Log("Writing to file: " + fullFile);
     return std::unique_ptr<std::ofstream> { new std::ofstream(fullFile, std::ofstream::out) };
 }
 
@@ -40,4 +49,20 @@ std::string FileOperations::ResourceRoot()
     const char *cStrResourceDir = [objcResourceDir fileSystemRepresentation];
     _resourceRoot = std::string(cStrResourceDir) + "/";
     return _resourceRoot;
+}
+
+std::string iOSFileOperations::DocumentsRoot() 
+{
+    NSArray *searchPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                       NSUserDomainMask, YES);
+    NSString *documentsPath = [searchPaths objectAtIndex:0];
+    std::string resPath( [ documentsPath UTF8String ] ) ;
+    return resPath + "/";
+}
+
+bool iOSFileOperations::IsFileInDocumentsFolder(const std::string & filePath)
+{
+    auto documentsFullFile = iOSFileOperations::DocumentsRoot() + filePath;
+    std::ifstream documentsFileStream(documentsFullFile.c_str());
+    return documentsFileStream.good();
 }
