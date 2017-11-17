368e5809cb11747fd8d62e81b99d2df3912f026c
Tucker Lein <self@tuckerle.in>
2017-11-16 23:14:45 -0600

    allow for TextDrawables to be copied

--- Engine/Src/Ancona/Core2D/Systems/Drawable/TextDrawable.hpp
+++ Engine/Src/Ancona/Core2D/Systems/Drawable/TextDrawable.hpp
@@ -19,6 +19,7 @@ class TextDrawable : public Drawable
          * @brief Default constructor, should only be used by the serializer.
          */
         TextDrawable() {}
+        TextDrawable(const std::string & text, const sf::Font * font);
 
         /**
          * @brief An element to draw text to an entity.
@@ -64,7 +65,13 @@ class TextDrawable : public Drawable
         void alpha(int alpha) override;
     private:
         std::unique_ptr<sf::Text> _text;
+        std::string _fontKey = "";
+        sf::Color _color = sf::Color::White;
+        int _characterSize = 10;
+        bool _smooth = true;
 
+
+        void SetupText();
         void CenterOrigin();
         void OnDraw(sf::RenderWindow &window, sf::Transform drawableTransform, float delta) override;
 };
