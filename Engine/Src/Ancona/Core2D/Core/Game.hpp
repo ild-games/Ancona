ca39feff50e4bc3a20f29d28c41ff6f909193ba4
tlein <tlein@radford.edu>
2018-04-20 19:16:59 -0500

    resolution independence

--- Engine/Src/Ancona/Core2D/Core/Game.hpp
+++ Engine/Src/Ancona/Core2D/Core/Game.hpp
@@ -1,54 +1,51 @@
 #ifndef Ancona_Engine_Core_Game_H_
 #define Ancona_Engine_Core_Game_H_
 
-#include <string>
-#include <SFML/Graphics.hpp>
 #include <Ancona/Framework/Screens/ScreenManager.hpp>
+#include <SFML/Graphics.hpp>
+#include <string>
 
-namespace ild
-{
+namespace ild {
 
 /**
  * @brief  Manages the game loop and game state
  *
  * @author Tucker Lein
  */
-class Game
-{
-    public:
-        /**
-         * @brief Construct the Game
-         */
-        Game(int windowWidth, int windowHeight, const std::string & title, const sf::Uint32 & style = sf::Style::Default);
+class Game {
+public:
+    /**
+     * @brief Construct the Game
+     */
+    Game(int windowWidth, int windowHeight, const std::string& title, const sf::Uint32& style = sf::Style::Default);
 
-        /**
-         * @brief Begin the game loop, run the game
-         */
-        void Run();
+    /**
+     * @brief Begin the game loop, run the game
+     */
+    void Run();
 
-        virtual ~Game();
+    virtual ~Game();
 
-        /* getters and setters */
-        const bool & windowIsActive() const { return _windowIsActive; }
-        ScreenManager & screenManager() { return *_screenManager; } 
+    /* getters and setters */
+    const bool& windowIsActive() const { return _windowIsActive; }
+    ScreenManager& screenManager() { return *_screenManager; }
 
-    protected:
-        /**
-         * @brief Instance of the game manager
-         */
-        ScreenManager * _screenManager;
+protected:
+    /**
+     * @brief Instance of the game manager
+     */
+    ScreenManager* _screenManager;
 
-        /**
-         * @brief Window used for rendering the game
-         */
-        sf::RenderWindow _window;
+    /**
+     * @brief Window used for rendering the game
+     */
+    sf::RenderWindow _window;
 
-    private:
-        void ProcessWindowEvent(sf::Event event);
+private:
+    void ProcessWindowEvent(sf::Event event);
 
-        bool _windowIsActive = true;
+    bool _windowIsActive = true;
 };
-
 }
 
 #endif
--- Engine/Src/Ancona/Core2D/Systems/CameraSystem.hpp
+++ Engine/Src/Ancona/Core2D/Systems/CameraSystem.hpp
@@ -11,8 +11,7 @@
 #include <Ancona/Framework/EntityFramework/UnorderedSystem.hpp>
 #include <Ancona/Framework/Serializing/Serializing.hpp>
 
-namespace ild
-{
+namespace ild {
 
 class DrawableSystem;
 
@@ -21,130 +20,130 @@ class DrawableSystem;
  *
  * @author Tucker Lein
  */
-class CameraComponent
-{
-    public:
-        /**
-         * @brief Default constructor, should only be used by the serializer.
-         */
-        CameraComponent();
+class CameraComponent {
+public:
+    /**
+     * @brief Default constructor, should only be used by the serializer.
+     */
+    CameraComponent();
 
-        /**
-         * @brief Constructs a new CameraComponent.
-         *
-         * @param originalView Default view of the window.
-         * @param renderPriority priority to render the camera by.
-         * @param scale The scale the camera will zoom to, defaults to 1.0f.
-         * @param offset The offset the camera will center on, defaults to {0, 0}
-         * @param lowerBounds The min position the camera can be in for the x and y axis
-         * @param upperBounds The max position the camera can be in for the x and y axis
-         */
-        CameraComponent(
-                const sf::View & originalView,
-                float renderPriority,
-                DrawableSystem * drawableSystem,
-                float scale = 1.0f,
-                sf::Vector2f offset = sf::Vector2f(0, 0),
-                sf::Vector2f lowerBounds = sf::Vector2f(std::numeric_limits<float>::min(), std::numeric_limits<float>::min()),
-                sf::Vector2f upperBounds = sf::Vector2f(std::numeric_limits<float>::max(), std::numeric_limits<float>::max()));
+    /**
+     * @brief Constructs a new CameraComponent.
+     *
+     * @param originalView Default view of the window.
+     * @param renderPriority priority to render the camera by.
+     * @param scale The scale the camera will zoom to, defaults to 1.0f.
+     * @param offset The offset the camera will center on, defaults to {0, 0}
+     * @param lowerBounds The min position the camera can be in for the x and y axis
+     * @param upperBounds The max position the camera can be in for the x and y axis
+     */
+    CameraComponent(
+        const sf::View& originalView,
+        float renderPriority,
+        DrawableSystem* drawableSystem,
+        float scale = 1.0f,
+        sf::Vector2f offset = sf::Vector2f(0, 0),
+        sf::Vector2f lowerBounds = sf::Vector2f(std::numeric_limits<float>::min(), std::numeric_limits<float>::min()),
+        sf::Vector2f upperBounds = sf::Vector2f(std::numeric_limits<float>::max(), std::numeric_limits<float>::max()));
 
-        /**
-         * @brief Overridable destructor.
-         */
-        virtual ~CameraComponent() { }
+    /**
+     * @brief Overridable destructor.
+     */
+    virtual ~CameraComponent() {}
 
-        /**
-         * @brief Updates the position of the camera.
-         */
-        void Update(float delta);
+    /**
+     * @brief Updates the position of the camera.
+     */
+    void Update(float delta);
 
-        /**
-         * @brief Draws the camera and all renderables on it.
-         *
-         * @param window RenderWindow for the game.
-         */
-        void Draw(sf::RenderTarget & target, float delta);
+    /**
+     * @brief Draws the camera and all renderables on it.
+     *
+     * @param window RenderWindow for the game.
+     */
+    void Draw(sf::RenderTarget& target, float delta);
 
-        /**
-         * @brief Adds a DrawableComponent to the camera's render queue.
-         *
-         * @param drawable DrawableComponent to add.
-         */
-        void AddDrawableComponent(DrawableComponent * drawable);
+    /**
+     * @brief Adds a DrawableComponent to the camera's render queue.
+     *
+     * @param drawable DrawableComponent to add.
+     */
+    void AddDrawableComponent(DrawableComponent* drawable);
 
-        /**
-         * @brief Remove a DrawableComponent from the camera's render queue.
-         *
-         * @param drawable DrawableComponent to remove.
-         */
-        void RemoveDrawableComponent(DrawableComponent * drawable);
+    /**
+     * @brief Remove a DrawableComponent from the camera's render queue.
+     *
+     * @param drawable DrawableComponent to remove.
+     */
+    void RemoveDrawableComponent(DrawableComponent* drawable);
 
-        /**
-         * @brief Called before the component is used to gather any dependcies necessary for it to operate.
-         *
-         * @param entity Entity the component is associated with.
-         */
-        void FetchDependencies(const Entity & entity);
+    /**
+     * @brief Called before the component is used to gather any dependcies necessary for it to operate.
+     *
+     * @param entity Entity the component is associated with.
+     */
+    void FetchDependencies(const Entity& entity);
 
-        /**
-         * @brief Responsible for saving and loading the object.
-         *
-         * @param arc Archive instance during the save/load process.
-         */
-        void Serialize(Archive & arc);
+    /**
+     * @brief Responsible for saving and loading the object.
+     *
+     * @param arc Archive instance during the save/load process.
+     */
+    void Serialize(Archive& arc);
 
-        /* getters and setters */
-        float renderPriority() { return _renderPriority; }
-        void follows(Entity follows);
-        void scale(float scale);
-        float scale() { return _scale; }
-        void lowerBounds(const sf::Vector2f & lowerBounds) { _lowerBounds = lowerBounds; }
-        const sf::Vector2f & lowerBounds() { return _lowerBounds; }
-        void upperBounds(const sf::Vector2f & upperBounds) { _upperBounds = upperBounds; }
-        const sf::Vector2f & upperBounds() { return _upperBounds; }
-        const sf::View & view() { return _view; }
+    /* getters and setters */
+    float renderPriority() { return _renderPriority; }
+    void follows(Entity follows);
+    void scale(float scale);
+    float scale() { return _scale; }
+    void lowerBounds(const sf::Vector2f& lowerBounds) { _lowerBounds = lowerBounds; }
+    const sf::Vector2f& lowerBounds() { return _lowerBounds; }
+    void upperBounds(const sf::Vector2f& upperBounds) { _upperBounds = upperBounds; }
+    const sf::Vector2f& upperBounds() { return _upperBounds; }
+    const sf::View& view() { return _view; }
 
-    protected:
-        /**
-         * @brief Position component for what the camera is following.
-         */
-        PositionComponent * _followPosition = nullptr;
+protected:
+    /**
+     * @brief Position component for what the camera is following.
+     */
+    PositionComponent* _followPosition = nullptr;
 
-        /**
-         * @brief Gets the true center position for the camera that will determine the location of the camera
-         *        each frame. By default this will handle such things as:
-         *          * following the follows entity
-         *          * applying offset
-         *          * applying bounds
-         * @returns Vector for the true position of the camera
-         */
-        virtual sf::Vector2f GetEffectiveCenter();
+    /**
+     * @brief Gets the true center position for the camera that will determine the location of the camera
+     *        each frame. By default this will handle such things as:
+     *          * following the follows entity
+     *          * applying offset
+     *          * applying bounds
+     * @returns Vector for the true position of the camera
+     */
+    virtual sf::Vector2f GetEffectiveCenter();
 
-    private:
-        /**
-         * @brief SFML view for actually applying the camera's position on the window.
-         */
-        sf::View _view;
-        /**
-         * @brief priority in which the camera is rendered, lower priority means  it will be rendered sooner.
-         */
-        float _renderPriority;
-        /**
-         * @brief Queue for the drawables on the camera, sorted by the drawable's render priority + render offset.
-         */
-        std::vector<DrawableComponent *> _renderQueue;
-        float _scale = 1;
-        float _originalScale = 1;
-        sf::Vector2f _offset;
-        Entity _follows = nullentity;
-        sf::Vector2f _size;
-        sf::Vector2f _lowerBounds;
-        sf::Vector2f _upperBounds;
-        PositionSystem * _positionSystem;
-        DrawableSystem * _drawableSystem;
-        bool _default = false;
-        bool _sorted = false;
+private:
+    /**
+     * @brief SFML view for actually applying the camera's position on the window.
+     */
+    sf::View _view;
+    /**
+     * @brief priority in which the camera is rendered, lower priority means  it will be rendered sooner.
+     */
+    float _renderPriority;
+    /**
+     * @brief Queue for the drawables on the camera, sorted by the drawable's render priority + render offset.
+     */
+    std::vector<DrawableComponent*> _renderQueue;
+    float _scale = 1;
+    float _originalScale = 1;
+    sf::Vector2f _offset;
+    Entity _follows = nullentity;
+    sf::Vector2f _size;
+    sf::Vector2f _lowerBounds;
+    sf::Vector2f _upperBounds;
+    PositionSystem* _positionSystem;
+    DrawableSystem* _drawableSystem;
+    bool _default = false;
+    bool _sorted = false;
 
+    void ApplyLetterboxView(int windowWidth, int windowHeight);
 };
 
 /**
@@ -152,43 +151,40 @@ class CameraComponent
  *
  * @author Tucker Lein
  */
-class CameraSystem : public UnorderedSystem<CameraComponent>
-{
-    public:
-        /**
-         * @brief Constructs the CameraSystem.
-         *
-         * @param name Name of the system.
-         * @param manager SystemManager instance for the current screen.
-         */
-        CameraSystem(
-                std::string name,
-                SystemManager & manager);
+class CameraSystem : public UnorderedSystem<CameraComponent> {
+public:
+    /**
+     * @brief Constructs the CameraSystem.
+     *
+     * @param name Name of the system.
+     * @param manager SystemManager instance for the current screen.
+     */
+    CameraSystem(
+        std::string name,
+        SystemManager& manager);
 
-        /**
-         * @brief Updates the components managed by the system.
-         */
-        void Update(float delta);
+    /**
+     * @brief Updates the components managed by the system.
+     */
+    void Update(float delta);
 
-        /**
-         * @brief Constructs a CameraComponent and attaches it to the system.
-         *
-         * @param entity Entity to associate the component with.
-         * @param originalView Default view of the window.
-         * @param renderPriority The render priority of the camera.
-         * @param scale The scale the camera will zoom to, defaults to 1.0f.
-         *
-         * @return Pointer to the CameraComponent being created.
-         */
-        CameraComponent * CreateComponent(
-                const Entity & entity,
-                const sf::View & originalView,
-                float renderPriority,
-                DrawableSystem * drawableSystem,
-                float scale = 1.0f);
-
+    /**
+     * @brief Constructs a CameraComponent and attaches it to the system.
+     *
+     * @param entity Entity to associate the component with.
+     * @param originalView Default view of the window.
+     * @param renderPriority The render priority of the camera.
+     * @param scale The scale the camera will zoom to, defaults to 1.0f.
+     *
+     * @return Pointer to the CameraComponent being created.
+     */
+    CameraComponent* CreateComponent(
+        const Entity& entity,
+        const sf::View& originalView,
+        float renderPriority,
+        DrawableSystem* drawableSystem,
+        float scale = 1.0f);
 };
-
 }
 
 #endif
