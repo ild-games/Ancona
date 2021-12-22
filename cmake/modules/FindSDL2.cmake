89796a567236de2577029de4ab234bc803173682
Tucker Lein <self@tuckerle.in>
2021-12-21 19:12:56 -0500

    macbook

--- cmake/modules/FindSDL2.cmake
+++ cmake/modules/FindSDL2.cmake
@@ -1,23 +1,25 @@
 # forward the SDL2 include directory so SDL_Image includes work
 include_directories("${RAFT_INCLUDE_DIR}/SDL2")
 
+if(CMAKE_BUILD_TYPE STREQUAL "DEBUG")
+    set(LIB_SUFFIX "d")
+else()
+    set(LIB_SUFFIX "")
+endif()
+
 # Setup the correct spot where SDL is built to for the SDL libs
 add_library(SDL2 SHARED IMPORTED GLOBAL)
-if(CMAKE_BUILD_TYPE STREQUAL "DEBUG")
-    set_property(TARGET SDL2 PROPERTY IMPORTED_LOCATION "${RAFT_INSTALL_DIR}/bin/SDL2d.dll")
-    if (MSVC)
-        set_property(TARGET SDL2 PROPERTY IMPORTED_IMPLIB "${RAFT_LIB_DIR}/SDL2d.lib")
-    else()
-        set_property(TARGET SDL2 PROPERTY IMPORTED_IMPLIB "${RAFT_LIB_DIR}/libSDL2d.dll.a")
-    endif()
-else()
-    set_property(TARGET SDL2 PROPERTY IMPORTED_LOCATION "${RAFT_INSTALL_DIR}/bin/SDL2.dll")
-    if (MSVC)
-        set_property(TARGET SDL2 PROPERTY IMPORTED_IMPLIB "${RAFT_LIB_DIR}/SDL2.lib")
-    else()
-        set_property(TARGET SDL2 PROPERTY IMPORTED_IMPLIB "${RAFT_LIB_DIR}/libSDL2.dll.a")
-    endif()
+if (MSVC)
+    set_property(TARGET SDL2 PROPERTY IMPORTED_LOCATION "${RAFT_INSTALL_DIR}/bin/SDL2${LIB_SUFFIX}.dll")
+    set_property(TARGET SDL2 PROPERTY IMPORTED_IMPLIB "${RAFT_LIB_DIR}/SDL2${LIB_SUFFIX}.lib")
+elseif(WIN32)
+    set_property(TARGET SDL2 PROPERTY IMPORTED_LOCATION "${RAFT_INSTALL_DIR}/bin/SDL2${LIB_SUFFIX}.dll")
+    set_property(TARGET SDL2 PROPERTY IMPORTED_IMPLIB "${RAFT_LIB_DIR}/libSDL2${LIB_SUFFIX}.dll.a")
+elseif(APPLE)
+    set_property(TARGET SDL2 PROPERTY IMPORTED_LOCATION "${RAFT_LIB_DIR}/libSDL2${LIB_SUFFIX}.dylib")
+    set_property(TARGET SDL2 PROPERTY IMPORTED_IMPLIB "${RAFT_LIB_DIR}/libSDL2${LIB_SUFFIX}.a")
 endif()
+
 
 # SDL_Image uses the SDL2::SDL2 alias, so set this up as well
 add_library(SDL2::SDL2 ALIAS SDL2)
